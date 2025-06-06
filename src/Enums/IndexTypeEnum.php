ae0b88115261e9f4983510619efd8069a1a039cd
Алексей Зорин <zorinalexey59292@gmail.com>
2025-06-07 02:37:27 +0400

    start schema builder

--- src/Enums/IndexTypeEnum.php
+++ src/Enums/IndexTypeEnum.php
@@ -2,7 +2,12 @@
 
 namespace CloudCastle\SqlBuilder\Enums;
 
-enum IndexTypeEnum
+enum IndexTypeEnum: string
 {
-    
+    case BTREE = 'BTREE';
+    case HASH = 'HASH';
+    case FULLTEXT = 'FULLTEXT';
+    case CLUSTERED = 'CLUSTERED';
+    case UNIQUE = 'UNIQUE';
+    case INDEX = 'INDEX';
 }
